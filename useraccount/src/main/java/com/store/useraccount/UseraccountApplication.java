78d3794209bf23fe2d1528080016f742ff4a9cd2
shreeraj <karkishreeraj@gmail.com>
2017-12-04 13:14:32 -0600

    independent projects created for each modules'

---
+++ useraccount/src/main/java/com/store/useraccount/UseraccountApplication.java
@@ -0,0 +1,12 @@
+package com.store.useraccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseraccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UseraccountApplication.class, args);
+	}
+}
