6c0833e41e03ad0c92cf9bce96320bdeb93ec6b8
shreeraj <karkishreeraj@gmail.com>
2017-12-04 12:11:46 -0600

    created spring boot project

---
+++ src/main/java/com/electronicstore/app/ElectronicstoreApplication.java
@@ -0,0 +1,12 @@
+package com.electronicstore.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicstoreApplication.class, args);
+	}
+}
